0633d760fe78ee831e8fcde1101ca5c34013c521
Yaroslav <elorenn@bk.ru>
2026-01-24 21:20:44 +0300

    Update SettingsActivity.cpp

--- src/activities/settings/SettingsActivity.cpp
+++ src/activities/settings/SettingsActivity.cpp
@@ -37,7 +37,6 @@ const SettingInfo readerSettings[readerSettingsCount] = {
 
 constexpr int controlsSettingsCount = 4;
 const SettingInfo controlsSettings[controlsSettingsCount] = {
-    SettingInfo::Enum(
     SettingInfo::Enum("Front Button Layout", &CrossPointSettings::frontButtonLayout,
                       {"Bck, Cnfrm, Lft, Rght", "Lft, Rght, Bck, Cnfrm", "Lft, Bck, Cnfrm, Rght"}),
     SettingInfo::Enum("Side Button Layout (reader)", &CrossPointSettings::sideButtonLayout,
