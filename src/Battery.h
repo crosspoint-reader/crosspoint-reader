b8b55a68af0048cfade07b2dbd188607ee40a60b
didacta <igor.yermak@gmail.com>
2026-01-24 22:34:23 -0500

    style: fix clang-format issue in Battery.h

--- src/Battery.h
+++ src/Battery.h
@@ -8,6 +8,4 @@ static BatteryMonitor battery(BAT_GPIO0);
 
 // Returns true when USB is connected (device is charging)
 // Uses GPIO20 (U0RXD) which reads HIGH when USB is connected
-inline bool isCharging() {
-  return digitalRead(20) == HIGH;
-}
+inline bool isCharging() { return digitalRead(20) == HIGH; }
