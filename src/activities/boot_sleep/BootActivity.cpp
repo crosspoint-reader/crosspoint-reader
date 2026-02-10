a2787d1ba573b305148529bed54d4b389849ddb9
ztrawhcs <matthew.s.schwartz@gmail.com>
2026-02-10 10:11:54 -0500

    Fix formatting in BootActivity.cpp

--- src/activities/boot_sleep/BootActivity.cpp
+++ src/activities/boot_sleep/BootActivity.cpp
@@ -15,8 +15,7 @@ void BootActivity::onEnter() {
   renderer.drawImage(Logo120, (pageWidth - 120) / 2, (pageHeight - 120) / 2, 120, 120);
 
   // Custom Mod Title
-  renderer.drawCenteredText(UI_10_FONT_ID, pageHeight / 2 + 70, "Crosspoint-Button-Mod", true,
-                            EpdFontFamily::BOLD);
+  renderer.drawCenteredText(UI_10_FONT_ID, pageHeight / 2 + 70, "Crosspoint-Button-Mod", true, EpdFontFamily::BOLD);
 
   renderer.drawCenteredText(SMALL_FONT_ID, pageHeight / 2 + 95, "BOOTING");
 
