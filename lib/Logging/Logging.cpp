a4e47ecd87157a73f85a49bada52858633ef5d0e
Xuan Son Nguyen <son@huggingface.co>
2026-02-12 23:05:19 +0100

    clang format

--- lib/Logging/Logging.cpp
+++ lib/Logging/Logging.cpp
@@ -1,27 +1,28 @@
 #include "Logging.h"
 
 // Since logging can take a large amount of flash, we want to make the format string as short as possible.
-// This logPrintf prepend the timestamp, level and origin to the user-provided message, so that the user only needs to provide the format string for the message itself.
+// This logPrintf prepend the timestamp, level and origin to the user-provided message, so that the user only needs to
+// provide the format string for the message itself.
 void logPrintf(const char* level, const char* origin, const char* format, ...) {
   if (!Serial) {
-    return; // Serial not initialized, skip logging
+    return;  // Serial not initialized, skip logging
   }
   va_list args;
   va_start(args, format);
   char buf[256];
-  char *c = buf;
+  char* c = buf;
   // add the timestamp
   {
     unsigned long ms = millis();
     int len = snprintf(c, sizeof(buf), "[%lu] ", ms);
     if (len < 0) {
-      return; // encoding error, skip logging
+      return;  // encoding error, skip logging
     }
     c += len;
   }
   // add the level
   {
-    const char *p = level;
+    const char* p = level;
     size_t remaining = sizeof(buf) - (c - buf);
     while (*p && remaining > 1) {
       *c++ = *p++;
@@ -35,7 +36,7 @@ void logPrintf(const char* level, const char* origin, const char* format, ...) {
   {
     int len = snprintf(c, sizeof(buf) - (c - buf), "[%s] ", origin);
     if (len < 0) {
-      return; // encoding error, skip logging
+      return;  // encoding error, skip logging
     }
     c += len;
   }
