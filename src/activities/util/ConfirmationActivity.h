330b0eb56efc10e50fb69eafdbb5bf38c530fa39
Lev Roland-Kalb <levrolandkalb@gmail.com>
2026-02-16 15:13:50 -0500

    Fixed Formatting
    Fixed Formatting

--- src/activities/util/ConfirmationActivity.h
+++ src/activities/util/ConfirmationActivity.h
@@ -1,18 +1,18 @@
 #pragma once
-#include "../Activity.h"
 #include <functional>
 #include <string>
 
+#include "../Activity.h"
+
 class ConfirmationActivity : public Activity {
-public:
-    ConfirmationActivity(GfxRenderer& renderer, MappedInputManager& mappedInput,
-                         const std::string& message,
-                         std::function<void(bool)> onResult); 
+ public:
+  ConfirmationActivity(GfxRenderer& renderer, MappedInputManager& mappedInput, const std::string& message,
+                       std::function<void(bool)> onResult);
 
-    void onEnter() override;
-    void loop() override;
+  void onEnter() override;
+  void loop() override;
 
-private:
-    std::string message;
-    std::function<void(bool)> onResult;
+ private:
+  std::string message;
+  std::function<void(bool)> onResult;
 };
