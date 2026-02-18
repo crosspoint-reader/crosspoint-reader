66ad6723400fb5cd9c30d2b05d97816c5506f2a6
LSTAR <lstar1900@gmail.com>
2026-02-18 20:47:20 +0000

    feat: Implement silent indexing for the next chapter in EpubReaderActivity

--- src/activities/reader/EpubReaderActivity.h
+++ src/activities/reader/EpubReaderActivity.h
@@ -27,6 +27,7 @@ class EpubReaderActivity final : public ActivityWithSubactivity {
   void renderContents(std::unique_ptr<Page> page, int orientedMarginTop, int orientedMarginRight,
                       int orientedMarginBottom, int orientedMarginLeft);
   void renderStatusBar(int orientedMarginRight, int orientedMarginBottom, int orientedMarginLeft) const;
+  void silentIndexNextChapterIfNeeded(uint16_t viewportWidth, uint16_t viewportHeight);
   void saveProgress(int spineIndex, int currentPage, int pageCount);
   // Jump to a percentage of the book (0-100), mapping it to spine and page.
   void jumpToPercent(int percent);
