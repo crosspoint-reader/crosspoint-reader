4ae1e40a2a6e2169c3e8cbca81050eeb521ea732
Eliz Kilic <elizk@google.com>
2026-02-26 20:32:35 +0000

    Prompt activity

--- src/activities/util/BmpViewerActivity.cpp
+++ src/activities/util/BmpViewerActivity.cpp
@@ -8,13 +8,16 @@
 #include <algorithm>
 
 #include "CrossPointSettings.h"
+#include "PromptActivity.h"
 #include "components/UITheme.h"
 #include "fontIds.h"
 #include "util/StringUtils.h"
 
 BmpViewerActivity::BmpViewerActivity(GfxRenderer& renderer, MappedInputManager& mappedInput, std::string path,
                                      std::function<void()> onGoBack)
-    : Activity("BmpViewer", renderer, mappedInput), filePath(std::move(path)), onGoBack(std::move(onGoBack)) {}
+    : ActivityWithSubactivity("BmpViewer", renderer, mappedInput),
+      filePath(std::move(path)),
+      onGoBack(std::move(onGoBack)) {}
 
 void BmpViewerActivity::loadSiblingImages() {
   siblingImages.clear();
@@ -109,22 +112,14 @@ void BmpViewerActivity::onEnter() {
       }
 
       // 4. Prepare Rendering
-      MappedInputManager::Labels labels;
-      if (isConfirmingDelete) {
-        labels = mappedInput.mapLabels(tr(STR_CANCEL), tr(STR_CONFIRM), "", "");
-      } else {
-        labels = mappedInput.mapLabels(tr(STR_BACK), tr(STR_SET_SLEEP_COVER), tr(STR_DELETE), "");
-      }
+      const auto labels = mappedInput.mapLabels(tr(STR_BACK), tr(STR_SET_SLEEP_COVER), tr(STR_DELETE), "");
+
       GUI.fillPopupProgress(renderer, popupRect, 50);
 
       renderer.clearScreen();
       // Assuming drawBitmap defaults to 0,0 crop if omitted, or pass explicitly: drawBitmap(bitmap, x, y, pageWidth,
       // pageHeight, 0, 0)
       renderer.drawBitmap(bitmap, x, y, pageWidth, pageHeight, 0, 0);
-
-      if (isConfirmingDelete) {
-        GUI.drawPopup(renderer, tr(STR_DELETE_IMAGE_PROMPT));
-      }
 
       // Draw UI hints on the base layer
       GUI.drawButtonHints(renderer, labels.btn1, labels.btn2, labels.btn3, labels.btn4);
@@ -159,79 +154,33 @@ void BmpViewerActivity::onExit() {
 }
 
 void BmpViewerActivity::loop() {
-  // Keep CPU awake/polling so 1st click works
-  Activity::loop();
+  if (subActivity) {
+    subActivity->loop();
+    return;
+  }
 
   if (mappedInput.wasReleased(MappedInputManager::Button::Back)) {
-    if (isConfirmingDelete) {
-      isConfirmingDelete = false;
-      onEnter();
-    } else {
-      if (onGoBack) onGoBack();
-    }
+    if (onGoBack) onGoBack();
     return;
   }
 
   if (mappedInput.wasReleased(MappedInputManager::Button::Confirm)) {
-    if (isConfirmingDelete) {
-      GUI.drawPopup(renderer, tr(STR_LOADING_POPUP));
-
-      if (Storage.remove(filePath.c_str())) {
-        GUI.drawPopup(renderer, tr(STR_DONE));
-        delay(1000);
-        if (onGoBack) onGoBack();
-      } else {
-        GUI.drawPopup(renderer, tr(STR_FAILED_LOWER));
-        delay(1000);
-        isConfirmingDelete = false;
-        onEnter();
-      }
-      return;
-    }
-
-    GUI.drawPopup(renderer, tr(STR_LOADING_POPUP));
-
-    bool success = false;
-    FsFile inFile, outFile;
-    if (Storage.openFileForRead("BMP", filePath, inFile)) {
-      if (Storage.openFileForWrite("BMP", "/sleep.bmp", outFile)) {
-        char buffer[2048];
-        int bytesRead;
-        success = true;
-        while ((bytesRead = inFile.read(buffer, sizeof(buffer))) > 0) {
-          if (outFile.write(buffer, bytesRead) != bytesRead) {
-            success = false;
-            break;
-          }
-        }
-        outFile.close();
-      }
-      inFile.close();
-    }
-
-    if (success) {
-      SETTINGS.sleepScreen = CrossPointSettings::SLEEP_SCREEN_MODE::CUSTOM;
-      SETTINGS.saveToFile();
-      GUI.drawPopup(renderer, tr(STR_DONE));
-    } else {
-      GUI.drawPopup(renderer, tr(STR_FAILED_LOWER));
-    }
-
-    delay(1000);
-    onEnter();
+    doSetSleepCover();
     return;
   }
 
   if (mappedInput.wasReleased(MappedInputManager::Button::Left)) {
-    if (!isConfirmingDelete) {
-      isConfirmingDelete = true;
-      onEnter();
-    }
+    enterNewActivity(new PromptActivity(
+        renderer, mappedInput, tr(STR_DELETE_IMAGE_PROMPT), [this] { doDelete(); },
+        [this] {
+          exitActivity();
+          onEnter();
+        }));
     return;
   }
 
   if (mappedInput.wasReleased(MappedInputManager::Button::Up)) {
-    if (!isConfirmingDelete && siblingImages.size() > 1 && currentImageIndex > 0) {
+    if (siblingImages.size() > 1 && currentImageIndex > 0) {
       currentImageIndex--;
       std::string dirPath = "/";
       size_t lastSlash = filePath.find_last_of('/');
@@ -247,7 +196,7 @@ void BmpViewerActivity::loop() {
   }
 
   if (mappedInput.wasReleased(MappedInputManager::Button::Down)) {
-    if (!isConfirmingDelete && siblingImages.size() > 1 && currentImageIndex != -1 &&
+    if (siblingImages.size() > 1 && currentImageIndex != -1 &&
         currentImageIndex < static_cast<int>(siblingImages.size()) - 1) {
       currentImageIndex++;
       std::string dirPath = "/";
@@ -262,4 +211,52 @@ void BmpViewerActivity::loop() {
     }
     return;
   }
+}
+
+void BmpViewerActivity::doDelete() {
+  exitActivity();
+  GUI.drawPopup(renderer, tr(STR_LOADING_POPUP));
+
+  if (Storage.remove(filePath.c_str())) {
+    GUI.drawPopup(renderer, tr(STR_DONE));
+    delay(1000);
+    if (onGoBack) onGoBack();
+  } else {
+    GUI.drawPopup(renderer, tr(STR_FAILED_LOWER));
+    delay(1000);
+    onEnter();
+  }
+}
+
+void BmpViewerActivity::doSetSleepCover() {
+  GUI.drawPopup(renderer, tr(STR_LOADING_POPUP));
+
+  bool success = false;
+  FsFile inFile, outFile;
+  if (Storage.openFileForRead("BMP", filePath, inFile)) {
+    if (Storage.openFileForWrite("BMP", "/sleep.bmp", outFile)) {
+      char buffer[2048];
+      int bytesRead;
+      success = true;
+      while ((bytesRead = inFile.read(buffer, sizeof(buffer))) > 0) {
+        if (outFile.write(buffer, bytesRead) != bytesRead) {
+          success = false;
+          break;
+        }
+      }
+      outFile.close();
+    }
+    inFile.close();
+  }
+
+  if (success) {
+    SETTINGS.sleepScreen = CrossPointSettings::SLEEP_SCREEN_MODE::CUSTOM;
+    SETTINGS.saveToFile();
+    GUI.drawPopup(renderer, tr(STR_DONE));
+  } else {
+    GUI.drawPopup(renderer, tr(STR_FAILED_LOWER));
+  }
+
+  delay(1000);
+  onEnter();
 }
--- src/activities/util/BmpViewerActivity.h
+++ src/activities/util/BmpViewerActivity.h
@@ -3,24 +3,24 @@
 #include <functional>
 #include <string>
 
-#include "../Activity.h"
+#include "../ActivityWithSubactivity.h"
 #include "MappedInputManager.h"
 
-class BmpViewerActivity final : public Activity {
+class BmpViewerActivity final : public ActivityWithSubactivity {
  public:
   BmpViewerActivity(GfxRenderer& renderer, MappedInputManager& mappedInput, std::string filePath,
                     std::function<void()> onGoBack);
 
   void onEnter() override;
-  void onExit() override;
   void loop() override;
 
  private:
   void loadSiblingImages();
+  void doDelete();
+  void doSetSleepCover();
 
   std::string filePath;
   std::function<void()> onGoBack;
-  bool isConfirmingDelete = false;
   std::vector<std::string> siblingImages;
   int currentImageIndex = -1;
 };
