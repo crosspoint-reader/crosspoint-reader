8700a8dcadf658fd0afce78e9fed40cf7e15f826
jpirnay <jens@pirnay.com>
2026-02-27 15:14:30 +0100

    Stupid oversight

--- src/activities/settings/SettingsActivity.h
+++ src/activities/settings/SettingsActivity.h
@@ -8,7 +8,7 @@
 #include "activities/Activity.h"
 #include "util/ButtonNavigator.h"
 
-class CrossPointSettings;
+#include "CrossPointSettings.h"
 
 enum class SettingType { TOGGLE, ENUM, ACTION, VALUE, STRING };
 
