53d99d932a597460381277226888f60011f11036
Jakub Kolcar <webmjkfm@gmail.com>
2026-04-16 10:04:47 +0200

    Point OTA updater to fork releases

--- src/network/OtaUpdater.cpp
+++ src/network/OtaUpdater.cpp
@@ -8,7 +8,7 @@
 #include "esp_wifi.h"
 
 namespace {
-constexpr char latestReleaseUrl[] = "https://api.github.com/repos/crosspoint-reader/crosspoint-reader/releases/latest";
+constexpr char latestReleaseUrl[] = "https://api.github.com/repos/ioma8/crosspoint-ioma8/releases/latest";
 
 /* This is buffer and size holder to keep upcoming data from latestReleaseUrl */
 char* local_buf;
