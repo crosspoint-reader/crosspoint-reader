e77af1d21958f304e61b5ba0bcd6938f68ef42c0
ethan160222 <ethan.sadacca@gmail.com>
2026-04-23 16:16:33 -0700

    Revert "fix: Replaced Bookerly with Noto Serif for licensing reasons (#1736)"
    This reverts commit c5f82709c00f76ef4fdd1e8995400c7947549c94.

--- src/CrossPointSettings.cpp
+++ src/CrossPointSettings.cpp
@@ -225,7 +225,7 @@ bool CrossPointSettings::loadFromBinaryFile() {
 
 float CrossPointSettings::getReaderLineCompression() const {
   switch (fontFamily) {
-    case NOTOSERIF:
+    case BOOKERLY:
     default:
       switch (lineSpacing) {
         case TIGHT:
@@ -293,18 +293,18 @@ int CrossPointSettings::getRefreshFrequency() const {
 
 int CrossPointSettings::getReaderFontId() const {
   switch (fontFamily) {
-    case NOTOSERIF:
+    case BOOKERLY:
     default:
       switch (fontSize) {
         case SMALL:
-          return NOTOSERIF_12_FONT_ID;
+          return BOOKERLY_12_FONT_ID;
         case MEDIUM:
         default:
-          return NOTOSERIF_14_FONT_ID;
+          return BOOKERLY_14_FONT_ID;
         case LARGE:
-          return NOTOSERIF_16_FONT_ID;
+          return BOOKERLY_16_FONT_ID;
         case EXTRA_LARGE:
-          return NOTOSERIF_18_FONT_ID;
+          return BOOKERLY_18_FONT_ID;
       }
     case NOTOSANS:
       switch (fontSize) {
