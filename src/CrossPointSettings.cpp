5b4e0308b61872434ec4d54ea5c711e375c74134
Jackson Goode <54308792+jacksongoode@users.noreply.github.com>
2026-05-18 01:56:02 +0900

    Fix serialization order

--- src/CrossPointSettings.cpp
+++ src/CrossPointSettings.cpp
@@ -178,8 +178,6 @@ bool CrossPointSettings::loadFromBinaryFile() {
     if (++settingsRead >= fileSettingsCount) break;
     readAndValidate(inputFile, sideButtonLayout, SIDE_BUTTON_LAYOUT_COUNT);
     if (++settingsRead >= fileSettingsCount) break;
-    serialization::readPod(inputFile, frontButtonFollowOrientation);
-    if (++settingsRead >= fileSettingsCount) break;
     readAndValidate(inputFile, fontFamily, FONT_FAMILY_COUNT);
     if (++settingsRead >= fileSettingsCount) break;
     readAndValidate(inputFile, fontSize, FONT_SIZE_COUNT);
@@ -241,6 +239,8 @@ bool CrossPointSettings::loadFromBinaryFile() {
     serialization::readPod(inputFile, fadingFix);
     if (++settingsRead >= fileSettingsCount) break;
     serialization::readPod(inputFile, embeddedStyle);
+    if (++settingsRead >= fileSettingsCount) break;
+    serialization::readPod(inputFile, frontButtonFollowOrientation);
     if (++settingsRead >= fileSettingsCount) break;
   } while (false);
 
