d145b21a67f982e7efc960560bf5f7d0c80ff654
Uri Tauber <uritaube@gmail.com>
2026-08-09 17:28:04 +0300

    Revert "perf: cache cumulative spine sizes in RAM (#2441)"
    This reverts commit 136cff4b1849ad5e43f0fb69ce4b666539f8588c.

--- lib/Epub/Epub.cpp
+++ lib/Epub/Epub.cpp
@@ -802,12 +802,7 @@ int Epub::getSpineItemsCount() const {
   return bookMetadataCache->getSpineCount();
 }
 
-size_t Epub::getCumulativeSpineItemSize(const int spineIndex) const {
-  if (!bookMetadataCache || !bookMetadataCache->isLoaded()) {
-    return 0;
-  }
-  return bookMetadataCache->getCumulativeSize(spineIndex);
-}
+size_t Epub::getCumulativeSpineItemSize(const int spineIndex) const { return getSpineItem(spineIndex).cumulativeSize; }
 
 BookMetadataCache::SpineEntry Epub::getSpineItem(const int spineIndex) const {
   if (!bookMetadataCache || !bookMetadataCache->isLoaded()) {
