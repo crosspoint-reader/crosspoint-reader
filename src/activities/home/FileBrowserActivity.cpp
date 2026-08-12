f21127cc2342e79aa6e4176e74f8aeb89f60359b
Julia Nguyen <julia@uxj.io>
2026-08-12 12:46:57 -0400

    fix: display PNG files in device browser

--- src/activities/home/FileBrowserActivity.cpp
+++ src/activities/home/FileBrowserActivity.cpp
@@ -69,7 +69,7 @@ void FileBrowserActivity::loadFiles() {
         }
       } else if (FsHelpers::hasEpubExtension(filename) || FsHelpers::hasXtcExtension(filename) ||
                  FsHelpers::hasTxtExtension(filename) || FsHelpers::hasMarkdownExtension(filename) ||
-                 FsHelpers::hasBmpExtension(filename)) {
+                 FsHelpers::hasBmpExtension(filename) || FsHelpers::hasPngExtension(filename)) {
         files.emplace_back(filename);
       }
     }
