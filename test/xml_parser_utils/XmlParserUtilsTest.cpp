a7d5e2c6e99e517f5539992aab21d7c09be6745b
Piotr Siuszko <siuszko@zoho.com>
2026-08-16 20:29:08 +0200

    Reduced scope for tests

--- test/xml_parser_utils/XmlParserUtilsTest.cpp
+++ test/xml_parser_utils/XmlParserUtilsTest.cpp
@@ -2,24 +2,8 @@
 
 #include "XmlParserUtils.h"
 
-TEST(XmlParserUtils, MatchesUnprefixedAndPrefixedLocalNames) {
+TEST(XmlParserUtils, MatchesLocalNamesRegardlessOfPrefix) {
   EXPECT_TRUE(xmlLocalNameEquals("package", "package"));
-  EXPECT_TRUE(xmlLocalNameEquals("opf:package", "package"));
   EXPECT_TRUE(xmlLocalNameEquals("ns0:package", "package"));
-  EXPECT_TRUE(xmlLocalNameEquals("metadata", "metadata"));
-  EXPECT_TRUE(xmlLocalNameEquals("ns0:manifest", "manifest"));
-  EXPECT_TRUE(xmlLocalNameEquals("ns0:spine", "spine"));
-  EXPECT_TRUE(xmlLocalNameEquals("ns0:itemref", "itemref"));
-  EXPECT_TRUE(xmlLocalNameEquals("dc:title", "title"));
-}
-
-TEST(XmlParserUtils, RejectsDifferentLocalNames) {
-  EXPECT_FALSE(xmlLocalNameEquals("opf:metadata", "package"));
-  EXPECT_FALSE(xmlLocalNameEquals("manifest", "spine"));
-  EXPECT_FALSE(xmlLocalNameEquals("dc:creator", "title"));
-}
-
-TEST(XmlParserUtils, HandlesNullQName) {
-  EXPECT_STREQ(xmlLocalName(nullptr), "");
-  EXPECT_FALSE(xmlLocalNameEquals(nullptr, "package"));
+  EXPECT_FALSE(xmlLocalNameEquals("ns0:metadata", "package"));
 }
--- test/xml_parser_utils/stubs/freertos/semphr.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Host-test stub for FreeRTOS's semaphore handle type. The firmware only needs
-// the type to declare HalStorage::storageMutex; no semaphore API is exercised.
-
-using SemaphoreHandle_t = void*;
