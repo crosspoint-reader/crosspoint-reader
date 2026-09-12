d466ac8c531b1f3d3df5aa2c3bbf336f8912edd0
Terralyft <ricsonjaymadonza@gmail.com>
2026-09-12 22:16:29 +0800

    style: align short power settings formatting

--- src/SettingsList.h
+++ src/SettingsList.h
@@ -325,11 +325,10 @@ inline std::vector<SettingInfo> getSettingsList(const SdCardFontRegistry* regist
          StrId::STR_FOOTNOTES, StrId::STR_CONFIRM, StrId::STR_EOB_HOME},
                           "shortPwrBtn", StrId::STR_CAT_CONTROLS),
 #else
-        SettingInfo::Enum(
-            StrId::STR_SHORT_PWR_BTN, &CrossPointSettings::shortPwrBtn,
-      {StrId::STR_IGNORE, StrId::STR_SLEEP, StrId::STR_PAGE_TURN, StrId::STR_FORCE_REFRESH,
-       StrId::STR_FOOTNOTES, StrId::STR_EOB_HOME},
-            "shortPwrBtn", StrId::STR_CAT_CONTROLS),
+  SettingInfo::Enum(StrId::STR_SHORT_PWR_BTN, &CrossPointSettings::shortPwrBtn,
+        {StrId::STR_IGNORE, StrId::STR_SLEEP, StrId::STR_PAGE_TURN, StrId::STR_FORCE_REFRESH,
+         StrId::STR_FOOTNOTES, StrId::STR_EOB_HOME},
+        "shortPwrBtn", StrId::STR_CAT_CONTROLS),
 #endif
         SettingInfo::Toggle(StrId::STR_PWR_BTN_FOOTNOTE_BACK, &CrossPointSettings::pwrBtnFootnoteBack,
                             "pwrBtnFootnoteBack", StrId::STR_CAT_CONTROLS),
